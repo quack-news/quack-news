1f7ca4d52856caec76913e6b037feb8555db8e2e
imgabe <gabrielpmonte@hotmail.com>
2026-01-12 13:00:09 -0300

    feat: adicionar Keystatic CMS para gestão de conteúdo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
+import react from '@astrojs/react';
+import keystatic from '@keystatic/astro';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://quack-news.github.io',
   base: '/',
   trailingSlash: "always",
-  integrations: [svelte()]
+  integrations: [svelte(), react(), keystatic()]
 });
